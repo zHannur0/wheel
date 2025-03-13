75628f3b06d59e0aba629daf25e5cdcac8408587
Zhannurkhan Arystambek <94554791+zHannur0@users.noreply.github.com>
2025-03-13 17:25:41 +0500

    end

--- src/components/SpinCircle.tsx
+++ src/components/SpinCircle.tsx
@@ -30,21 +30,25 @@ const categories = [
 
 const CategoriesTable = () => {
     return (
-        <div className="flex justify-center w-1/2">
-            <div className="overflow-x-auto">
-                <table className="table-auto border-collapse border border-gray-400 text-sm md:text-base ">
-                    <thead>
-                    <tr className="bg-[#BAFFD4] text-gray-900">
-                        <th className="border border-gray-500 px-2 py-1 rounded-lt-md">RoboLand 2025 санаттары</th>
+        <div className="flex flex-col items-center gap-2 w-1/2">
+            <h1 className="font-bold text-xl">RoboLand 2025</h1>
+
+            <div className="overflow-hidden rounded-lg border border-1 ">
+                <table className="table-auto rounded-lg border-separate border-spacing-0 w-full text-sm md:text-base">
+                    <thead className="bg-[#BAFFD4] text-gray-900">
+                    <tr>
+                        <th className="border border-gray-500 px-2 py-1 rounded-tl-lg">RoboLand 2025 санаттары</th>
                         <th className="border border-gray-500 px-2 py-1">Categories of the RoboLand 2025</th>
-                        <th className="border border-gray-500 px-2 py-1 rounded-rt-md">RoboLand 2025 категории</th>
+                        <th className="border border-gray-500 px-2 py-1 rounded-tr-lg">RoboLand 2025 категории</th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="bg-[#BAFFD4]">
                     {categories.map((row, index) => (
-                        <tr key={index} className="bg-[#BAFFD4]">
+                        <tr key={index}>
                             {row.map((item, i) => (
-                                <td key={i} className="border border-gray-400 px-2 py-1 text-xs">{index + 1}. {item}</td>
+                                <td key={i} className="border border-gray-400 px-2 py-1 text-xs">
+                                    {index + 1}. {item}
+                                </td>
                             ))}
                         </tr>
                     ))}
@@ -54,5 +58,7 @@ const CategoriesTable = () => {
         </div>
     );
 };
+
+
 
 export default CategoriesTable;
--- src/components/WinnerModal.tsx
+++ src/components/WinnerModal.tsx
@@ -2,28 +2,28 @@ import React, {useEffect, useRef} from "react";
 import Image from "next/image";
 
 const categories = [
-    ["3D прототиптеу 1 деңгей", "3D prototyping level 1", "3D прототипирование 1 уровень"],
-    ["3D прототиптеу 2 деңгей", "3D prototyping level 2", "3D прототипирование 2 уровень"],
+    ["3D прототипирование Level 1", "3D prototyping Level 1", "3D прототиптеу Level 1"],
+    ["3D прототипирование Level 2", "3D prototyping Level 2", "3D прототиптеу Level 2"],
     ["Fischertechnik", "Fischertechnik", "Fischertechnik"],
     ["Roboland-Kazakhstan", "Roboland-Kazakhstan", "Roboland-Kazakhstan"],
-    ["Үлкен саяхат", "Great journey", "Большое путешествие"],
-    ["Кегельринг-квадро х 2", "Keglering Quadro X2", "Кегельринг-квадро х 2"],
-    ["Лабиринт 1 деңгей", "Labyrinth level 1", "Лабиринт 1 уровень"],
-    ["Лабиринт 2 деңгей", "Labyrinth level 2", "Лабиринт 2 уровень"],
-    ["Су роботтарының лабиринті", "Aquatic robot labyrinth", "Лабиринт водных роботов"],
-    ["Басқарылатын квадрокоптерлерді маневрлеу", "Maneuvering of controlled quadcopters", "Маневрирование управляемых квадрокоптеров"],
-    ["Басқарылатын роботтардың шағын футболы", "Football of controlled robots", "Мини-футбол управляемых роботов"],
-    ["Жұптық теннис", "Pair tennis", "Парный теннис"],
-    ["Кедергілер жолағы", "Obstacle course", "Полоса препятствий"],
-    ["Робогеометрия 1 деңгей", "Robogeometry level 1", "Робогеометрия 1 уровень"],
-    ["Робогеометрия 2 деңгей", "Robogeometry level 2", "Робогеометрия 2 уровень"],
-    ["Роботы жинақтау 1 деңгей", "Build a robot level 1", "Собери робота 1 уровень"],
-    ["Роботы жинақтау 2 деңгей", "Build a robot level 2", "Собери робота 2 уровень"],
-    ["Садақ ату", "Archery", "Стрельба из лука"],
-    ["«Робот-құтқарушылар» шығармашылық санаты 1 деңгей", "Creative category 'Rescuer robots' level 1", "Творческая категория 'Роботы-спасатели' 1 уровень"],
-    ["«Робот-құтқарушылар» шығармашылық санаты 2 деңгей", "Creative category 'Rescuer robots' level 2", "Творческая категория 'Роботы-спасатели' 2 уровень"],
-    ["«Робот-құтқарушылар» шығармашылық санаты 3 деңгей", "Creative category 'Rescuer robots' level 3", "Творческая категория 'Роботы-спасатели' 3 уровень"],
-    ["Тенге алу", "Tenge alu", "Тенге алу"]
+    ["Большое путешествие", "Great journey", "Үлкен саяхат"],
+    ["Кегельринг-квадро х 2", "Kegelring Quadro X2", "Кегельринг-квадро х 2"],
+    ["Лабиринт Level 1", "Labyrinth Level 1", "Лабиринт Level 1"],
+    ["Лабиринт Level 2", "Labyrinth Level 2", "Лабиринт Level 2"],
+    ["Лабиринт водных роботов", "Aquatic robot labyrinth", "Су роботтарының лабиринті"],
+    ["Маневрирование управляемых квадрокоптеров", "Manoeuvring of controlled quadcopters", "Басқарылатын квадрокоптерлерді маневрлеу"],
+    ["Мини-футбол управляемых роботов", "Football of controlled robots", "Басқарылатын роботтардың шағын футболы"],
+    ["Парный теннис", "Pair tennis", "Жұптық теннис"],
+    ["Полоса препятствий", "Obstacle course", "Кедергілер жолағы"],
+    ["Робогеометрия Level 1", "Robogeometry Level 1", "Робогеометрия Level 1"],
+    ["Робогеометрия Level 2", "Robogeometry Level 2", "Робогеометрия Level 2"],
+    ["Собери робота Level 1", "Build a robot Level 1", "Роботты жинаңыз Level 1"],
+    ["Собери робота Level 2", "Build a robot Level 2", "Роботты жинаңыз Level 2"],
+    ["Садақ ату", "Sadak aty", "Садақ ату"],
+    ["Творческая категория «Роботы-спасатели» Level 1", "Creative category 'Rescuer robots' Level 1", "«Робот-құтқарушылар» шығармашылық санаты Level 1"],
+    ["Творческая категория «Роботы-спасатели» Level 2", "Creative category 'Rescuer robots' Level 2", "«Робот-құтқарушылар» шығармашылық санаты Level 2"],
+    ["Творческая категория «Роботы-спасатели» Level 3", "Creative category 'Rescuer robots' Level 3", "«Робот-құтқарушылар» шығармашылық санаты Level 3"],
+    ["Теңге алу", "Tenge alu", "Теңге алу"]
 ];
 
 const WinnerModal = ({ winnerIndex, onClose }: {winnerIndex:number, onClose: () => void}) => {
@@ -52,14 +52,16 @@ const WinnerModal = ({ winnerIndex, onClose }: {winnerIndex:number, onClose: () 
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/10  bg-opacity-10 z-50">
-            <div ref={modalRef} className="bg-[#E8EAF6] text-black p-6 rounded-md shadow-lg text-center relative w-[70%] h-[70%]  max-h-[787px] flex flex-col items-center justify-center rounded-[25px]">
+            <div ref={modalRef}
+                 className="bg-[#E8EAF6] text-black p-6 rounded-md shadow-lg text-center relative w-[70%] h-[70%]  max-h-[787px] flex flex-col items-center justify-center rounded-[25px]">
                 <h2 className="text-4xl">
                     Таңдалған санат | Выбранная категория | Selected category
                 </h2>
 
                 {/* Кружок с номером */}
                 <div className="flex justify-center my-4">
-                    <div className="w-[200px] h-[200px] flex items-center justify-center border border-black rounded-full text-[96px] font-bold bg-white">
+                    <div
+                        className="w-[200px] h-[200px] flex items-center justify-center border border-black rounded-full text-[96px] font-bold bg-white">
                         {winnerIndex + 1}
                     </div>
                 </div>
@@ -70,13 +72,15 @@ const WinnerModal = ({ winnerIndex, onClose }: {winnerIndex:number, onClose: () 
                 <p className="text-4xl mb-5">{winnerIndex + 1}. {categories[winnerIndex][2]}</p>
 
                 {/* Декоративные конфетти */}
-                <div className="absolute left-5 top-0 w-20 h-full flex items-center">
-                    <Image src={"/left.png"} alt="RoboLand Logo" width={122} height={385} />
+                <div className="absolute left-5 top-0 w-20 h-full flex items-center animate-slide-in-left">
+                    <Image src={"/left.png"} alt="RoboLand Logo" width={122} height={385}/>
                 </div>
-                <div className="absolute right-5 top-0 w-20 h-full flex items-center justify-end">
-                    <Image src={"/right.png"} alt="RoboLand Logo" width={122} height={385} />
 
+                <div
+                    className="absolute right-5 top-0 w-20 h-full flex items-center justify-end animate-slide-in-right">
+                    <Image src={"/right.png"} alt="RoboLand Logo" width={122} height={385}/>
                 </div>
+
 
                 {/* Кнопка закрытия */}
                 {/*<button*/}
